e89e5ed465017fcd98f9159897f4f3d10abf1b1d
Vlad Savin <vlad24077@gmail.com>
2023-03-03 15:50:01 +0300

    Init commit.

---
+++ Source/ShootAndRun/ShootAndRun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootAndRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootAndRun, "ShootAndRun" );
