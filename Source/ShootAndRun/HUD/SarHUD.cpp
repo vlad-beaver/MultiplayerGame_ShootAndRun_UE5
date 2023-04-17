a9f26c26b8f66ea422f40c79accd03a37bfafcd5
Vlad Savin <vlad24077@gmail.com>
2023-04-17 12:30:11 +0300

    [Weapon Aim Mechanics] Drawing the Crosshairs.

--- Source/ShootAndRun/HUD/SarHUD.cpp
+++ Source/ShootAndRun/HUD/SarHUD.cpp
@@ -7,5 +7,54 @@ void ASarHUD::DrawHUD()
 {
 	Super::DrawHUD();
 
-	
+	FVector2D ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2D ViewportCenter(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
+
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBottom, ViewportCenter);
+		}
+	}
+}
+
+void ASarHUD::DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter)
+{
+	const float TextureWidth = Texture->GetSizeX();
+	const float TextureHeight = Texture->GetSizeY();
+	const FVector2D TextureDrawPoint(
+		ViewportCenter.X - (TextureWidth / 2.f),
+		ViewportCenter.Y - (TextureHeight / 2.f)
+	);
+
+	DrawTexture(
+		Texture,
+		TextureDrawPoint.X,
+		TextureDrawPoint.Y,
+		TextureWidth,
+		TextureHeight,
+		0.f,
+		0.f,
+		1.f,
+		1.f,
+		FLinearColor::White
+		);
 }
