7586fd5c558a277c5ce441ede6f07a70dacd3b98
DESKTOP-28D9RD6\ivan_test <beavervs2407@gmail.com>
2023-06-04 06:31:59 +0300

    [Polishing] Recrate starter map & lobby.

--- Source/ShootAndRun/SarComponents/CombatComponent.cpp
+++ Source/ShootAndRun/SarComponents/CombatComponent.cpp
@@ -388,6 +388,7 @@ void UCombatComponent::TraceUnderCrosshairs(FHitResult& TraceHitResult)
 			End,
 			ECollisionChannel::ECC_Visibility
 		);
+		if (!TraceHitResult.bBlockingHit) TraceHitResult.ImpactPoint = End;
 		if (TraceHitResult.GetActor() && TraceHitResult.GetActor()->Implements<UInteractWithCrosshairsInterface>())
 		{
 			HUDPackage.CrosshairsColor = FLinearColor::Red;
